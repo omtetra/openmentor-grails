1909754b2182568a38d20c960e9011276e01aed2
morungos <stuart@morungos.com>
2012-04-22 20:19:14 -0400

    Updated ExtractStandard to use POI's WordExtractor directly rather than the manually hacked up Java code. This allows us to use the anonymized data files for testing, which (due to being generated by LibreOffice or Pages) were breaking the old code.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -29,7 +29,8 @@ grails.project.dependency.resolution = {
 
         runtime 'mysql:mysql-connector-java:5.1.18'
 		
-		compile 'org.apache.poi:poi:3.7'
+		compile 'org.apache.poi:poi:3.8'
+		compile 'org.apache.poi:poi-scratchpad:3.8'
 		
 		test    'org.htmlparser:htmlparser:1.6'
 		test    'org.gmock:gmock:0.8.2'
