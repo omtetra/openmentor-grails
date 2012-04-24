70f65b13b158cf1ea96c49428b0fcaa6d711b199
morungos <stuart@morungos.com>
2012-04-23 20:54:39 -0400

    Added initial code for the history view - #12

--- grails-app/controllers/uk/org/openmentor/controller/HistoryController.groovy
+++ grails-app/controllers/uk/org/openmentor/controller/HistoryController.groovy
@@ -1,5 +1,7 @@
 package uk.org.openmentor.controller
 
+import uk.org.openmentor.data.Submission;
+
 /**
  * This controller is used to manage the views and actions associated with the
  * history. The precise contents of this history may evolve over time, but it 
@@ -11,6 +13,26 @@ package uk.org.openmentor.controller
 class HistoryController {
 
     def index() { 
-		
+		redirect(action: "list", params: params)
 	}
+	
+	def list = {
+		params.max = Math.min(params.max ? params.int('max') : 10, 100)
+		params.sort = params.sort ?: 'dateSubmitted'
+		params.order = params.order ?: 'asc'
+		params.offset = params.offset ?: '0'
+				
+		def criteria = Submission.createCriteria()
+		
+		def submissionList = criteria.list {
+			order(params.sort, params.order)
+			maxResults(params.max)
+			firstResult(Integer.parseInt(params.offset))
+		}
+		
+		def submissionCount = Submission.count()
+		
+		[submissionInstanceList: submissionList, submissionInstanceTotal: submissionCount]
+	}
+
 }
