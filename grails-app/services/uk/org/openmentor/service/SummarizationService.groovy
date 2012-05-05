8732ac99736c272571aa6db0d373538fa8a51f4f
morungos <stuart@192.168.101.214>
2012-05-05 16:38:10 -0400

    Added extra documentation to the SummarizationService. This replaces the AssessmentService which was previously totally inexplicable #10

--- grails-app/services/uk/org/openmentor/service/SummarizationService.groovy
+++ grails-app/services/uk/org/openmentor/service/SummarizationService.groovy
@@ -21,26 +21,50 @@ import uk.org.openmentor.util.MultiMap;
  */
 class SummarizationService {
 	
+	/**
+	 * Returns a Summary for the whole collection.
+	 * @return
+	 */
 	def getSummary(Boolean comments = false) {
 		makeSummary([], [], [], [:], comments)
 	}
 	
+	/**
+	 * Returns a Summary for a specified course.
+	 * @return
+	 */
 	def getCourseSummary(String course, Boolean comments = false) {
 		makeSummary([], ["sub.assignment as ass"], ["ass.courseId = :courseId"], ["courseId": course], comments)
 	}
 
+	/**
+	 * Returns a Summary for a specified course, broken down by assignment.
+	 * @return
+	 */
 	def getCourseSummaryByAssignment(String course, Boolean comments = false) {
 		makeSummary(["ass.code"], ["sub.assignment as ass"], ["ass.courseId = :courseId"], ["courseId": course], comments)
 	}
 
+	/**
+	 * Returns a Summary for a specified course, broken down by tutor.
+	 * @return
+	 */
 	def getCourseSummaryByTutor(String course, Boolean comments = false) {
 		makeSummary(["tutor_id"], ["sub.assignment as ass", "sub.tutorIds as tutor_id"], ["ass.courseId = :courseId"], ["courseId": course], comments)
 	}
 
+	/**
+	 * Returns a Summary for a specified course, broken down by student.
+	 * @return
+	 */
 	def getCourseSummaryByStudent(String course, Boolean comments = false) {
 		makeSummary(["student_id"], ["sub.assignment as ass", "sub.studentIds as student_id"], ["ass.courseId = :courseId"], ["courseId": course], comments)
 	}
 
+	/**
+	 * Returns a Summary for a single submission.
+	 * @return
+	 */
 	def getSubmissionSummary(Submission submission, Boolean comments = false) {
 		makeSummary([], ["sub.assignment as ass"], ["sub.id = :id"], ["id": submission.id], comments)
 	}
@@ -64,6 +88,10 @@ class SummarizationService {
 		String whereString = criteriaString ? "where ${criteriaString}" : ""
 		String whereAndString = criteriaString ? "and ${criteriaString}" : ""
 		
+		/*
+		 * HQL to generate a query generating counts for actual comments and submissions
+		 * based on the specified join and filter. 
+		 */
 		String countQuery = "\
 select new map(count(distinct sub.id) as submissionCount, count(distinct comment.id) as commentCount) \
 from Submission sub \
@@ -72,6 +100,10 @@ from Submission sub \
 ${joinString} \
 ${whereString}"
 				
+		/*
+		 * HQL to generate a query generating ideal counts for the submissions
+		 * based on the specified join and filter. 
+		 */
 		String idealQuery = "\
 select new list(${dimensionsString} weight.band, round(weight.weight * count(distinct comment.id))) \
 from Submission sub, Weight weight \
@@ -82,6 +114,10 @@ ${whereAndString} \
 group by ${dimensionsString} weight.band \
 order by ${dimensionsString} weight.band"
 		
+		/*
+		 * HQL to generate a query generating actual counts for the submissions
+		 * based on the specified join and filter. 
+		 */
 		String actualQuery = "\
 select new list(${dimensionsString} cat.band, count(distinct comment.id)) \
 from Submission sub \
@@ -98,6 +134,12 @@ order by ${dimensionsString} cat.band"
 		
 		def actualResult = Submission.executeQuery(actualQuery, namedParams)
 		
+		/*
+		 * Dump the row data into a hierarchical structure in a MultiMap, this
+		 * is keyed by the return values in order, expecting the band and 
+		 * the count to be the last values. Create a summary for the leaf 
+		 * objects.
+		 */
 		MultiMap values = new MultiMap()
 		for(List row in idealResult) {
 			def next = values
@@ -108,6 +150,13 @@ order by ${dimensionsString} cat.band"
 			next.putAt(row[keyCount - 1], new SummaryEntry(ideal: row.last()))
 		}
 
+		/*
+		 * Add the row data into a hierarchical structure in a MultiMap, this
+		 * is keyed by the return values in order, expecting the band and 
+		 * the count to be the last values. This time we can assume the 
+		 * summary entry exists. Always do actual after ideal, as ideal must
+		 * be present for all bands, and creates the summary.
+		 */
 		for(List row in actualResult) {
 			def next = values
 			def keyCount = row.size() - 1
@@ -117,6 +166,11 @@ order by ${dimensionsString} cat.band"
 			next.getAt(row[keyCount - 1]).actual = row.last()
 		}
 		
+		/*
+		 * If we are doing comments too, and this is an optional boolean parameter
+		 * that defaults to false, then grab the comments and add them into the
+		 * same structure. 
+		 */
 		if (comments) {
 			String commentQuery = "\
 select new list(${dimensionsString} cat.band, comment.text) \
@@ -139,6 +193,9 @@ order by ${dimensionsString} cat.band"
 			}
 		}
 
+		/*
+		 * And finally, return a new Summary. 
+		 */
 		return new Summary(
 			dimensions: dimensions, 
 			data: values,
