616dc3e466c94740aee385d818fa2958381d2d30
morungos <stuart@morungos.com>
2012-12-06 21:29:45 -0500

    Updated to Grails 2.1.1

--- src/java/uk/org/openmentor/extractor/impl/ExtractStandard.java
+++ src/java/uk/org/openmentor/extractor/impl/ExtractStandard.java
@@ -28,10 +28,8 @@ import uk.org.openmentor.extractor.Extractor;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.regex.Pattern;
 
