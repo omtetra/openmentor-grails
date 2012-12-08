9dbc8e4feff1f360847ffdfe61f915993323d07e
morungos <stuart@morungos.com>
2012-12-08 18:11:46 -0500

    Allows students and tutors to be created, although not yet edited, because deeper issues show. Resolves the immediate concerns in #20, though

--- test/unit/uk/org/openmentor/courseinfo/StudentTests.groovy
+++ test/unit/uk/org/openmentor/courseinfo/StudentTests.groovy
@@ -3,15 +3,86 @@ package uk.org.openmentor.courseinfo
 import grails.test.*
 
 class StudentTests extends GrailsUnitTestCase {
+	
+	def student
+	
     protected void setUp() {
         super.setUp()
+		
+		mockDomain(Student)
+		student = new Student()
+
+		student.studentId = '0000000'
+		student.givenName = 'Morag'
+		student.familyName = 'Mungo'
+		
     }
 
     protected void tearDown() {
         super.tearDown()
     }
 
-    void testSomething() {
-
+	/**
+	 * Checks that the untouched student object passes validation
+	 */
+    void testValidation() {
+		def result = student.validate()
+		student.errors.allErrors.each { log.info it.toString() }
+		assertTrue result
+		assertFalse student.hasErrors()
     }
+
+	/**
+	 * Checks that invalid sample types fail validation
+	 */
+	void testStudentId() {
+		def old = student.studentId
+		
+		student.studentId = ''
+		assertFalse student.validate()
+		assertTrue student.hasErrors()
+		
+		student.studentId = old
+	}
+	
+	/**
+	 * Must always be zero when comparing with self
+	 */
+	void testCompareSelf() {
+		assertEquals(0, student.compareTo(student))
+	}
+
+	/**
+	 * Checks that the untouched student object passes validation
+	 */
+    void testCompareOrdering() {
+		def other = new Student()
+		other.studentId = '00000001'
+		other.givenName = 'Morag'
+		other.familyName = 'Nunavut'
+		
+		assertEquals(-1, student.compareTo(other))
+		assertEquals(1, other.compareTo(student))
+    }
+
+	/**
+	 *  Compare with invalid/empty fields, always after
+	 */
+	void testCompareBlank() {
+		def other = new Student()
+		other.studentId = '00000001'
+		
+		assertEquals(-1, student.compareTo(other))
+		assertEquals(1, other.compareTo(student))
+	}
+
+	/**
+	 *  Compare with invalid/empty fields, always after
+	 */
+	void testCompareTwoBlank() {
+		def other = new Student()
+		other.studentId = '00000001'
+		
+		assertEquals(0, other.compareTo(other))
+	}
 }
--- test/unit/uk/org/openmentor/courseinfo/TutorTests.groovy
+++ test/unit/uk/org/openmentor/courseinfo/TutorTests.groovy
@@ -3,15 +3,86 @@ package uk.org.openmentor.courseinfo
 import grails.test.*
 
 class TutorTests extends GrailsUnitTestCase {
+	
+	def tutor
+	
     protected void setUp() {
         super.setUp()
+		
+		mockDomain(Tutor)
+		tutor = new Tutor()
+
+		tutor.tutorId = '0000000'
+		tutor.givenName = 'Morag'
+		tutor.familyName = 'Mungo'
+
     }
 
     protected void tearDown() {
         super.tearDown()
     }
 
-    void testSomething() {
-
+	/**
+	 * Checks that the untouched tutor object passes validation
+	 */
+    void testValidation() {
+		def result = tutor.validate()
+		tutor.errors.allErrors.each { log.info it.toString() }
+		assertTrue result
+		assertFalse tutor.hasErrors()
     }
+
+	/**
+	 * Checks that invalid sample types fail validation
+	 */
+	void testTutorId() {
+		def old = tutor.tutorId
+		
+		tutor.tutorId = ''
+		assertFalse tutor.validate()
+		assertTrue tutor.hasErrors()
+		
+		tutor.tutorId = old
+	}
+	
+	/**
+	 * Must always be zero when comparing with self
+	 */
+	void testCompareSelf() {
+		assertEquals(0, tutor.compareTo(tutor))
+	}
+
+	/**
+	 * Checks that the untouched tutor object passes validation
+	 */
+    void testCompareOrdering() {
+		def other = new Tutor()
+		other.tutorId = '00000001'
+		other.givenName = 'Morag'
+		other.familyName = 'Nunavut'
+		
+		assertEquals(-1, tutor.compareTo(other))
+		assertEquals(1, other.compareTo(tutor))
+    }
+
+	/**
+	 *  Compare with invalid/empty fields, always after
+	 */
+	void testCompareBlank() {
+		def other = new Tutor()
+		other.tutorId = '00000001'
+		
+		assertEquals(-1, tutor.compareTo(other))
+		assertEquals(1, other.compareTo(tutor))
+	}
+
+	/**
+	 *  Compare with invalid/empty fields, always after
+	 */
+	void testCompareTwoBlank() {
+		def other = new Tutor()
+		other.tutorId = '00000001'
+		
+		assertEquals(0, other.compareTo(other))
+	}
 }
