d2c1d2b7ef154274a898205e2ef58f25424e1111
morungos <stuart@morungos.com>
2013-03-09 18:21:17 -0500

    Added more taglib tests, as a stepping stone to progressive enhancement tests for charting

--- test/integration/uk/org/openmentor/charts/TagLibTests.groovy
+++ test/integration/uk/org/openmentor/charts/TagLibTests.groovy
@@ -32,6 +32,15 @@ class TagLibTests extends LoadedSubmissionsTestCase {
 		assertTrue result.contains(""""categories":["TMA01","TMA02"]""")
 	}
 
+	void testAssignmentActualIdealChartTagLib() {
+		Summary summary = summarizationService.getCourseAndAssignmentSummary("CM2006", "TMA01")
+		
+		def taglib = new ActualIdealTagLib()
+		def result = taglib.actualIdealChart([summary: summary, ref: "ref"])
+		
+		assertTrue result.contains("""<script type="text/javascript">""")
+	}
+
 	void testTutorDifferenceChartTagLib() {
 		Summary summary = summarizationService.getCourseSummaryByTutor("CM2006")
 		
@@ -42,6 +51,15 @@ class TagLibTests extends LoadedSubmissionsTestCase {
 		assertTrue result.contains(""""categories":["M4000061","M4000062"]""")
 	}
 
+	void testTutorActualIdealChartTagLib() {
+		Summary summary = summarizationService.getCourseAndTutorSummary("CM2006", "M4000061")
+		
+		def taglib = new ActualIdealTagLib()
+		def result = taglib.actualIdealChart([summary: summary, ref: "ref"])
+		
+		assertTrue result.contains("""<script type="text/javascript">""")
+	}
+
 	void testStudentDifferenceChartTagLib() {
 		Summary summary = summarizationService.getCourseSummaryByStudent("CM2006")
 		
@@ -50,5 +68,14 @@ class TagLibTests extends LoadedSubmissionsTestCase {
 		
 		assertTrue result.contains("""<script type="text/javascript">""")
 		assertTrue result.contains(""""categories":["09000231","09000232"]""")
+	}
+
+	void testStudentActualIdealChartTagLib() {
+		Summary summary = summarizationService.getCourseAndStudentSummary("CM2006", "09000231")
+		
+		def taglib = new ActualIdealTagLib()
+		def result = taglib.actualIdealChart([summary: summary, ref: "ref"])
+		
+		assertTrue result.contains("""<script type="text/javascript">""")
 	}
 }
